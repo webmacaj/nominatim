7c4e4c2e7c0374c80d9c2530c1cd76e069201fda
Jakub Macaj <webmacaj@gmail.com>
2021-07-31 13:06:42 +0200

    castle address

--- example/search.php
+++ example/search.php
@@ -1,1 +1,7 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$nominatim = new \Webmacaj\Nominatim\NominatimApi();
+$result = $nominatim->search([
+    'query' => 'Castle, 811 06, Bratislava, Slovakia'
+]);
